044a904d25556fb717d19a99c26d45d1baa12f1e
lina <ambowers@usf.edu>
2024-10-01 15:02:44 -0400

    Create department class to manage employees and calculate salary

--- employee_management.js
+++ employee_management.js
@@ -9,5 +9,21 @@ class Employee {
     }
 getDetails () { // use getDetails function to return a string with the employees name, position and salary
     return `Employee ${this.name} works as a ${this.position} earning an annual salary of $${this.salary}.`
+    }
+};
+
+// Task 2 create a department class
+
+class Department {
+    constructor (name) {
+        this.name = name;
+        this.employees = [] // plural bc array
+    }
+addEmployee(employee) {
+    this.employees.push.apply(employee) // pushes the employee object to the employees array
 }
-}
+getDepartmentSalary () {
+    return this.employees.reduce((total, employee) => total + employee.salary, 0);
+    } // returns total salary of all employees in the department
+};
+
